a9c16b36cf50838fd62720888b27d41506298de6
Ianko Schipala <49158352+iankooo@users.noreply.github.com>
2020-11-15 22:50:19 +0200

    Add files via upload

---
+++ lucrarea5/item5/src/CMakeFiles/Item5_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Item5_project.dir/item5.cpp.o"
+  "Item5_project"
+  "Item5_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Item5_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
