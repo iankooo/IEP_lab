9d3992e9636197b7e339526bbe5c0f3d6864ac69
iankooo <ianko_radoslav@yahoo.com>
2020-11-19 19:49:48 +0200

    update lucrarea5

---
+++ lucrarea5/item11/src/CMakeFiles/Item11_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Item11_project.dir/item11.cpp.o"
+  "Item11_project"
+  "Item11_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Item11_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lucrarea5/item12/src/CMakeFiles/Item12_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Item12_project.dir/item12.cpp.o"
+  "Item12_project"
+  "Item12_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Item12_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
